fe833fb56f0e9fc2e79ea823197ebc493efeae63
Bas <bbuijsen@gmail.com>
2022-02-07 18:11:55 +0100

    bug fixes

---
+++ MssqlInfrastructure/Migrations/20220207164409_location nullable again.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MssqlInfrastructure.Migrations
+{
+    public partial class locationnullableagain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ MssqlInfrastructure/Migrations/20220207164440_location nullable again 2.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MssqlInfrastructure.Migrations
+{
+    public partial class locationnullableagain2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rides_Locations_DestinationId",
+                table: "Rides");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DestinationId",
+                table: "Rides",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rides_Locations_DestinationId",
+                table: "Rides",
+                column: "DestinationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rides_Locations_DestinationId",
+                table: "Rides");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DestinationId",
+                table: "Rides",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rides_Locations_DestinationId",
+                table: "Rides",
+                column: "DestinationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
